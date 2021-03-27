9179044b2fb83f978c52fcde743281452bed2c12
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-03-26 23:16:52 -0300

    Accessing data from the frontend

--- app/src/main/java/com/example/hierarchy_class/MainActivity.kt
+++ app/src/main/java/com/example/hierarchy_class/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.hierarchy_class
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hierarchy_class.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -10,7 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.calculatebt.setOnClickListener(calculateTip())
     }
+    fun calculateTip(){
+        val costOfServiceText = binding.costOfService.text.toString()
+        val cost = costOfServiceText.toDouble()
 
+        val chosenTip = binding.options.checkedRadioButtonId
+        val tip = when (chosenTip){
+            R.id.button_thirty_percent-> 0.30;
+            R.id.button_thirty_percent-> 0.20;
+            R.id.button_thirty_percent-> 0.15;
+            R.id.button_thirty_percent-> 0.10;
+            else -> 0.05
+        }
 
+        }
+    }
 }
