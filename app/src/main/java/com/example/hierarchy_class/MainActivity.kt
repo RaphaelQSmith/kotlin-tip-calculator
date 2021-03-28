dd87e36758f657d0861e15c356badaab5dbf8fbe
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-03-27 23:22:33 -0300

    Write calculateTip function and feed value to Frontend

--- app/src/main/java/com/example/hierarchy_class/MainActivity.kt
+++ app/src/main/java/com/example/hierarchy_class/MainActivity.kt
@@ -3,29 +3,35 @@ package com.example.hierarchy_class
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hierarchy_class.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.calculatebt.setOnClickListener(calculateTip())
+        binding.calculatebt.setOnClickListener{(calculateTip())}
+
     }
-    fun calculateTip(){
+    private fun calculateTip(){
         val costOfServiceText = binding.costOfService.text.toString()
         val cost = costOfServiceText.toDouble()
 
-        val chosenTip = binding.options.checkedRadioButtonId
-        val tip = when (chosenTip){
-            R.id.button_thirty_percent-> 0.30;
-            R.id.button_thirty_percent-> 0.20;
-            R.id.button_thirty_percent-> 0.15;
-            R.id.button_thirty_percent-> 0.10;
+        val tipMultiplier = when (binding.options.checkedRadioButtonId){
+            R.id.button_thirty_percent-> 0.30
+            R.id.button_twenty_percent-> 0.20
+            R.id.button_fifteen_percent-> 0.15
+            R.id.button_ten_percent-> 0.10
             else -> 0.05
         }
+        var tip = tipMultiplier * cost
+        val roundingUp = binding.roundupswitch.isChecked
+        if (roundingUp) tip = ceil(tip)
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.totaltip.text = getString(R.string.total_tip, formattedTip)
 
-        }
     }
 }
